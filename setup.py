03b274a3e19283567af9765e01ac19a33efa03c3
Linjie Wu <wulinjie_math@pku.edu.cn>
2019-05-06 23:15:12 +0800

    Change permission

--- setup.py
+++ setup.py
@@ -12,5 +12,13 @@ while line:
     line = f.readline()
 open('ProkaBIC-seq','w').write(tmp)
 
+CMD = 'chmod 775 ' + dir +'/ProkaBIC-seq'
+os.system(CMD)
+CMD = 'chmod 775 ' + dir +'/src/BICseq2-seg_v0.7.3/*.pl'
+os.system(CMD)
+CMD = 'chmod 775 ' + dir +'/src/BICseq2-norm_v0.2.6/*.pl'
+os.system(CMD)
+CMD = 'chmod 775 ' + dir +'/src/samtools-0.1.7a_getUnique-0.1.3/samtools'
+os.system(CMD)
 CMD = 'Rscript ' + dir+'/src/CheckRpackages.r'
 os.system(CMD)
