d679e640d9ff29566bfe6a728185f3a2e867b3f3
Linjie Wu <wulinjiestat@gmail.com>
2019-09-28 20:32:21 +0800

    Create AddMappability.r

---
+++ src/AddMappability.r
@@ -0,0 +1,8 @@
+### Add mappability
+Args = commandArgs()
+file = Args[6]
+binL = Args[7]
+
+CNVs = read.table(file,header=T,sep='\t') 
+CNVs$Mappability = (CNVs$end-CNVs$start + 1)/binL/CNVs$binNum
+write.table(CNVs,file=file,sep='\t',quote=F,row.names=F)
