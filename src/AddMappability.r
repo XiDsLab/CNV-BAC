2b6f88f8f4765440a8d9daeb3d70fe2b81b6ed7f
Linjie Wu <30398866+LinjieWu@users.noreply.github.com>
2019-10-24 14:03:04 +0800

    Update AddMappability.r

--- src/AddMappability.r
+++ src/AddMappability.r
@@ -4,5 +4,5 @@ file = Args[6]
 binL = as.numeric(Args[7])
 
 CNVs = read.table(file,header=T,sep='\t') 
-CNVs$Mappability = (CNVs$end-CNVs$start + 1)/binL/CNVs$binNum
+CNVs$Mappability = binL*CNVs$binNum/(CNVs$end-CNVs$start + 1)
 write.table(CNVs,file=file,sep='\t',quote=F,row.names=F)
